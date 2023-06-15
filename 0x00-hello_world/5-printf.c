ca3245f138c87149bbb44a6bf3cac02f206f431d
Timothy Emuze <etiboy10@gmail.com>
2023-06-15 15:15:30 -0700

    created C program that prints string with printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/* statement that prints string */
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
