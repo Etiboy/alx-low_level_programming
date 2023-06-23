f16b55c16b721dcfd6b2fdf9402d36b6b122884a
Timothy Emuze <etiboy10@gmail.com>
2023-06-23 03:04:49 -0700

    Fizz Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,19 +7,19 @@
  */
 int main(void)
 {
-	int n = 1;
+	int n;
 
-	while (n <= 100)
+	for (n = 1; n <= 100; n++)
 	{
-		if ((n % 3) == 0)
+		if (n % 3 == 0)
 		{
 			printf("Fizz");
 		}
-		else if ((n % 5) == 0)
+		else if (n % 5 == 0)
 		{
 			printf("Buzz");
 		}
-		else if ((n % 15) == 0)
+		else if ((n % 3) == 0 && (n % 5) == 0)
 		{
 			printf("FizzBuzz");
 		}
@@ -27,11 +27,11 @@ int main(void)
 		{
 			printf("%d", n);
 		}
+
 		if (n != 100)
 		{
 			printf(" ");
 		}
-		n++;
 	}
 	printf("\n");
 	return (0);
