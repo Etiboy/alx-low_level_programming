5ca1b07ff662d72c077a651146a8329d43e578fa
Timothy Emuze <etiboy10@gmail.com>
2023-06-26 11:46:02 -0700

    Add a function that takes a pointer to an int

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - a function that takes a pointer to an int
+ * @n: The pointer variable to int
+ *
+ * Return: 0 success
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
