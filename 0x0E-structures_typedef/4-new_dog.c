9cf371d1b71abaa08eadfe61ae7cc86ba544928f
Timothy Emuze <etiboy10@gmail.com>
2023-07-17 15:43:40 -0700

    Updated commit

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * new_dog -  a function that creates a new dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Return: 0 success
+ */
+
+dog_t *new_dog(char *name, float age, char *owner);
+{
+	dog_t *dog;
+}
---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,22 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * free_dog -  a function that free
+ * @d: dog to free
+ *
+ * Return: 0 success
+ */
+
+void free_dog(dog_t *d);
+{
+	if (d == NULL)
+	{
+		return;
+	}
+
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
